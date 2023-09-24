a02b2ed732239026a639293448ea962ff8585a56
Shashank Chaturvedi <113339511+ShashankChaturvedi07@users.noreply.github.com>
2023-09-24 10:04:07 +0530

    Add files via upload

---
+++ 11. Event Key Codes/app.js
@@ -0,0 +1,23 @@
+// window.addEventListener('keydown', (e) => {
+//     console.log(e);
+// })
+
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML = ` 
+    <div class="key">
+        ${event.key === ' '  ?  'Space' : event.key} 
+        <small>event.key</small>
+    </div>
+
+    <div class="key">
+        ${event.keyCode} 
+        <small>event.keycode</small>
+    </div>
+
+    <div class="key">
+        ${event.code} 
+        <small>event.code</small>
+    </div>`
+})
