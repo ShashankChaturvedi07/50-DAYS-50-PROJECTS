fa11aca5db76454faba0f2c19a71aea2254be2f5
Shashank Chaturvedi <113339511+ShashankChaturvedi07@users.noreply.github.com>
2023-11-11 09:47:13 +0530

    Add files via upload

---
+++ 40. Todo List/app.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('You have to write something');
+    }
+    else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+
+    // it will save the data in your local storage 
+    saveData();
+}
+
+listContainer.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+
+        saveData();
+
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentNode.remove();
+
+        saveData();
+    }
+});
+
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// for displaying data whenever we re-open the site
+
+function showData (){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showData();
